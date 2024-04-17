97dce13ea6ee08aa81d185884c65eee97c1b7247
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2024-04-18 01:45:07 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
-#!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
 
-"""A variable storage, an instance of FileStorage"""
-storage = FileStorage()
+#!/usr/bin/python3
+"""
+initialize the models package
+"""
+
+from os import getenv
+
+
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
