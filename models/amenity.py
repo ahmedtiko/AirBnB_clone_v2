9a32b2dcc90a5ad63f2ba6a74e3b5cf818506636
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2024-04-18 01:50:34 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,20 @@
-#!/usr/bin/python3
-'''class inherent of BaseModel'''
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    '''class amenity'''
-
-    name = ""
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes Amenity"""
