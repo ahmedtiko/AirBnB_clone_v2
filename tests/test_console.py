dd5be2f54922335367f53111ebaf25685b20ad1e
ahmedtiko <33915834+ahmedtiko@users.noreply.github.com>
2024-04-18 00:57:37 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 import unittest
+from unittest.mock import patch
 import console
 from console import HBNBCommand
 
 
-class test_console(unittest.TestCase):
+class TestConsole(unittest.TestCase):
 
     def create(self):
         return HBNBCommand()
@@ -17,3 +18,10 @@ class test_console(unittest.TestCase):
     def test_EOF(self):
         con = self.create()
         self.assertTrue(con.onecmd("EOF"))
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_create(self, mock_stdout):
+        con = self.create()
+        con.onecmd('create BaseModel')
+        output = mock_stdout.getvalue().strip()
+        self.assertTrue(output.startswith("** instance id missing **"))
